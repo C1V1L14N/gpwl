ac3f3605ce5594a40a8f08f4439c6bdd4e7ee4f9
Michael Anderson <michaelanderson@michaels-mini.home>
2020-12-31 17:56:02 +0000

    tidied code up

--- Client/src/components/pedalBoard.vue
+++ Client/src/components/pedalBoard.vue
@@ -1,44 +1,30 @@
 <template>
     <div id="pedal-board">
         <section class="pedal-select-container">
-   <label for="pedal-select">Select a Pedal:</label>
-    <select id="pedal-select" v-model="selectedPedal">
-      <option disabled value="">Select a Pedal</option>
-      <option v-for="pedal in draggablePedalList" :key="pedal.name" :value="pedal" v-on:select="makeDraggablePedalList">{{pedal.name}}</option>
-    </select>
-  </section>
-    <div class="col-3">
-        <div class="p-2 alert alert-warning">
+            <label for="pedal-select">Select a Pedal:</label>
+            <select id="pedal-select" v-model="selectedPedal">
+                <option disabled value="">Select a Pedal</option>
+                <option v-for="pedal in draggablePedalList" :key="pedal.name" :value="pedal" v-on:select="makeDraggablePedalList">{{pedal.name}}</option>
+            </select>
+        </section>
             <button v-on:click="makeDraggablePedalList">Add fave pedals</button>
-          <h3>Pedals</h3>
-          <div
-            class="list-group-pedal-row"
-            :list="draggablePedalList"
-            :grid="20"
-            group="tasks"
-          >
-            <div
-              class="list-group-item"
-              v-for="pedal in draggablePedalList"
-              :key="pedal.name"
-              :list="draggablePedalList"
-              :grid="20"
-            >
-            
-              <input type="image" :src="pedal.image" alt="image of pedal" width="50" height="60" v-on:click="addToOnPedalList(pedal)">
-            
-            </div>
-          </div>
-        </div>
-        </div>
-        <h3>Pedal Board</h3>
-          <!-- Backlog draggable component. Pass arrBackLog to list prop -->
-          <div
-            class="list-group-pedal-board"
-            :list="onBoardList"
-            group="tasks"
-          >
-            </div>
+            <h3>Pedals</h3>
+                <div class="list-group-pedal-row">
+                    <div
+                        class="list-group-item"
+                        v-for="pedal in draggablePedalList"
+                        :key="pedal.name"
+                        :list="draggablePedalList"
+                    >
+                        <input type="image" :src="pedal.image" alt="image of pedal" width="50" height="60" v-on:click="addToOnPedalList(pedal)">
+                    </div>
+                </div>
+            <h3>Pedal Board</h3>
+                <div
+                    class="list-group-pedal-board"
+                    :list="onBoardList"
+                >
+                </div>
             <movable
               v-for="pedal in onBoardList"
               :key="pedal.name"
@@ -46,50 +32,41 @@
             >
               <img :src="pedal.image" alt="image of pedal" width="50" height="60">
             
-          </movable>
+            </movable>
       
     </div>
 </template>
 
 <script>
 import pedalItem from './pedalItem.vue'
-import draggable from 'vuedraggable';
 
 export default {
     name: 'pedal-board',
     props: ['pedalList', 'pedal'],
     components: {
-        'pedal-item': pedalItem,
-        draggable
+        'pedal-item': pedalItem
     },
     data() {
     return {
-      arrBackLog: [],
       draggablePedalList: [],
       onBoardList: [],
       selectedPedal: null
-      }},
+        }
+      },
   mounted() {
       this.makeDraggablePedalList();
-  },
+            },
   methods: {
     makeDraggablePedalList() {
         this.pedalList.forEach(pedal => {
             this.draggablePedalList.push(pedal)
-        });     
+            });     
         },
     addToOnPedalList(pedal){
       this.onBoardList.push(pedal);
+        }    
     }
-               
-        
-            
-        },
-    options: {
-        dropzoneSelector: '.list-group-pedal-board',
-        showDropzoneAreas: true
-    }
-    }
+}
 
 </script>
 
